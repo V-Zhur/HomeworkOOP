722a7db83e325bc2848b7b3cd334029d6fd68658
V-Zhur <lera.jur@yandex.ru>
2025-09-14 17:38:29 +0300

    Initial version

--- src/main/java/FormDate.java
+++ src/main/java/FormDate.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
 public class FormDate {
+    public int day;
+    public int month;
+    public int year;
 }
--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public String subscription;
+
+    public FormDate birthday;
+
 }
